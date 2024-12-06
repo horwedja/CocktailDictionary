d9bad9dd5274d86878eec095d52adab151bbafb2
horwedja <ashham101@hotmail.com>
2024-12-06 07:49:23 +0200

    1521

--- js/auth.js
+++ js/auth.js
@@ -3,6 +3,10 @@ async function authenticate(email, password) {
     try {
         const authData = await pb.collection('users').authWithPassword(email, password);
         console.log("Token:", pb.authStore.token); // Log the token for debugging
+
+        // Redirect to the dashboard after login
+        window.location.href = './dashboard.html';
+
         return pb.authStore.token; // Return token to caller
     } catch (error) {
         console.error("Authentication failed:", error);
@@ -15,3 +19,4 @@ function getToken() {
 }
 
 window.Auth = { authenticate, getToken };
+console.log("auth.js loaded");
